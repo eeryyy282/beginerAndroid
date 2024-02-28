17c7aefcc590d518a89d24d6474a284bb71af646
eeryyy282 <airiagustus82@gmail.com>
2024-02-28 20:45:15 +0700

    test push

--- app/src/main/java/com/example/beginerandroid/MainActivity.kt
+++ app/src/main/java/com/example/beginerandroid/MainActivity.kt
@@ -8,4 +8,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onDelete(data: String, data2: Int): String {
+        if (true) {
+            var data = "Testing string 1 2 3"
+            var data2= 22
+        }
+        return data + data2
+    }
+
+
 }
