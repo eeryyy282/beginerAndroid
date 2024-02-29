9592e84216824a0b4208dc6b9ed8715db4eb0c09
eeryyy282 <airiagustus82@gmail.com>
2024-03-01 02:40:54 +0700

    Make package "Intent" with MoveActivity

--- app/src/main/java/com/example/beginerandroid/intent/LearnIntent.kt
+++ app/src/main/java/com/example/beginerandroid/intent/LearnIntent.kt
@@ -1,9 +1,11 @@
-package com.example.beginerandroid
+package com.example.beginerandroid.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.example.beginerandroid.R
 
 class LearnIntent : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,8 @@ class LearnIntent : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v?.id) {
             R.id.btn_move_activity -> {
-
+                val moveIntent = Intent(this@LearnIntent, MoveActivity::class.java)
+                startActivity(moveIntent)
             }
         }
     }
---
+++ app/src/main/java/com/example/beginerandroid/intent/MoveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beginerandroid.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beginerandroid.R
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
