04f6aca5374e2a0e539f3c3a71cdd17d8cd162fe
eeryyy282 <airiagustus82@gmail.com>
2024-03-02 04:41:31 +0700

    make function for buy button to come back to main activity but with startActivity methode

--- app/src/main/java/com/example/beginerandroid/viewandviewgroup/LearnViewAndViewGroup.kt
+++ app/src/main/java/com/example/beginerandroid/viewandviewgroup/LearnViewAndViewGroup.kt
@@ -1,12 +1,29 @@
 package com.example.beginerandroid.viewandviewgroup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.beginerandroid.MainActivity
 import com.example.beginerandroid.R
 
-class LearnViewAndViewGroup : AppCompatActivity() {
+class LearnViewAndViewGroup : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnBuy: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn_view_and_view_group)
+
+        btnBuy = findViewById(R.id.btn_buy_product)
+        btnBuy.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_buy_product -> {
+                val buyThisProduct = Intent(this@LearnViewAndViewGroup, MainActivity::class.java)
+                startActivity(buyThisProduct)
+            }
+        }
     }
 }
