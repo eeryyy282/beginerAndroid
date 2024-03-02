3d0fb47f8466fb2ac07671d191a64232d4e09ad3
eeryyy282 <airiagustus82@gmail.com>
2024-03-02 14:34:12 +0700

    view and view group with constraint layout

--- app/src/main/java/com/example/beginerandroid/viewandviewgroup/LearnViewAndViewGroup.kt
+++ app/src/main/java/com/example/beginerandroid/viewandviewgroup/LearnViewAndViewGroup.kt
@@ -10,12 +10,16 @@ import com.example.beginerandroid.R
 
 class LearnViewAndViewGroup : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnBuy: Button
+    private lateinit var btnConstraintLayout: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learn_view_and_view_group)
 
         btnBuy = findViewById(R.id.btn_buy_product)
         btnBuy.setOnClickListener(this)
+
+        btnConstraintLayout = findViewById(R.id.btn_constraint_layout)
+        btnConstraintLayout.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -23,6 +27,10 @@ class LearnViewAndViewGroup : AppCompatActivity(), View.OnClickListener {
             R.id.btn_buy_product -> {
                 val buyThisProduct = Intent(this@LearnViewAndViewGroup, MainActivity::class.java)
                 startActivity(buyThisProduct)
+            }
+            R.id.btn_constraint_layout -> {
+                val moveToConstraintLayout = Intent(this@LearnViewAndViewGroup, ViewAndViewGroupWithConstraintLayout::class.java)
+                startActivity(moveToConstraintLayout)
             }
         }
     }
